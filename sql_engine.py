773ec5c3d72f9f712482900d69a0408d8597371d
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2016-08-31 03:23:32 +0530

    Added Some new things

--- sql_engine.py
+++ sql_engine.py
@@ -3,26 +3,51 @@ Main runner file for sql engine
 """
 
 import sys
+import re
 
 from util import read_meta
 
 __author__ = 'harry-7'
-
 METAFILE = 'metadata.txt'
 
 
 def main():
     """ The Main function. Inititates the sql engine functioning"""
-    table_data = read_meta(METAFILE)
+    table_info = read_meta(METAFILE)
     queries = str(sys.argv[1]).split(';')
     for query in queries:
-        process_query(query, table_data)
+        process_query(query, table_info)
 
 
-def process_query(query, table_data):
+def process_query(query, table_info):
     """Processes the given query and prints the output"""
-    print query
-    print table_data
+    query = format_string(query)
+    lis = []
+    if check_for('from', query.split()):
+        lis = query.split('from')
+    else:
+        error_exit("Syntax Error: No Table Selected")
+    project = format_string(str(lis[0]))
+    condition = format_string(lis[1])
+
+    if not check_for('select', project.split().lower()):
+        error_exit("Syntax Error: No Select statement given")
+
+
+def check_for(string, lis):
+    """Checks whether string is in the list"""
+    return string in lis
+
+
+def format_string(string):
+    """Returns the query in a formatted manner removing unnecessary spaces"""
+    return (re.sub(' +', ' ', string)).strip()
+
+
+def error_exit(error):
+    """Prints the error to Stderr and exits the program"""
+    sys.stderr.write(error)
+    exit(-1)
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -5,19 +5,21 @@ __author__ = 'harry-7'
 
 
 def read_meta(file_name):
-    """ Reads the Metadata of the file and returns a dictionary containing the info about table"""
+    """ Reads the Metadata of the file
+    returns a dictionary containing the info about table
+    """
     meta_file = open(file_name, 'r')
     start = False
     table_name = ""
-    table_data = {}
+    table_info = {}
     for line in meta_file:
         line = line.strip()
         if line == '<begin_table>':
             start = True
         elif start:
             table_name = line
-            table_data[table_name] = []
+            table_info[table_name] = []
             start = False
         elif line != '<end_table>':
-            table_data[table_name].append(line)
-    return table_data
+            table_info[table_name].append(line)
+    return table_info
